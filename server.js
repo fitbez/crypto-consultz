082c872f93259019cc9d0300b3560a614e471f0f
fitsum belayneh <fitsu_alt@yahoo.com>
2018-03-27 00:23:25 -0400

    passport init

--- server.js
+++ server.js
@@ -37,6 +37,10 @@ app.use(session({
   resave:true
 }));
 
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Middleware for expressValidator
 app.use(expressValidator({
   errorFormatter : function(param, msg, value){
@@ -54,3 +58,25 @@ app.use(expressValidator({
    };
   }
 }));
+
+// conect flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next){
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// routes
+app.use('/', routes);
+app.use('/users', users);
+
+// set port
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'),function(){
+  console.log('Server started on port ' +app.get('port'));
+});
