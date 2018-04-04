dc209ad2b21354542048d39745d17d103afe8e2d
eliram9 <eliram9@gmail.com>
2018-04-04 13:20:28 -0400

    srever.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-Parser');
+var cookieParser = require('cookie-Parser');
 var exphbs = require('express-handlebars');
 var expressValidator = require('express-validator');
 var flash = require('connect-flash');
@@ -11,11 +12,23 @@ var mongo = require('mongodb');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/crypto-consultz')
 var db = mongoose.connection;
+
+//database
+db.on('error', err => {
+ console.log(`There was an error connecting to the database: ${err}`);
+});
+db.once('open', () => {
+ console.log(
+   `You have successfully connected to your mongo database`
+ );
+});
+
+//path of routes
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 // Initialize App
-var app =express();
+var app = express();
 
 //Setup Handlebars view engine
 app.set('views', path.join(__dirname, 'views'));
@@ -23,8 +36,9 @@ app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
 // bodyParser Middleware
-app.use(bodyParser.json());
+app.use(bodyParser.json()); //restoring in the DB in JSON format
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(cookieParser());
 
 // Set Static folder
 app.use(express.static(path.join(__dirname,'public')));
@@ -39,6 +53,7 @@ app.use(session({
 // Passport init
 app.use(passport.initialize());
 app.use(passport.session());
+
 // Middleware for expressValidator
 app.use(expressValidator({
   errorFormatter : function(param, msg, value){
@@ -55,18 +70,23 @@ app.use(expressValidator({
    };
   }
 }));
+
 // conect flash
 app.use(flash());
+
 // Global Vars
 app.use(function (req, res, next){
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
   next();
 });
+
 // routes
 app.use('/', routes);
 app.use('/users', users);
+
 // set port
 app.set('port', (process.env.PORT || 3000));
 app.listen(app.get('port'),function(){
